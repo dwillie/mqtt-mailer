90f13c4ffb7382c2f084d80bf78b8756e79b9b6c
Rhys <rimmington@gmail.com>
2016-07-14 10:29:48 +1000

    CONFIG: Pass user.name to mkService (now mandatory)

--- service.nix
+++ service.nix
@@ -7,5 +7,6 @@ mkService {
     MAILER_ADDRESS = "YOUR_USERNAME@gmail.com";
     MAILER_PWD = "YOUR_PASSWORD";
   };
+  user.name = "mqtt-mailer";
   script = "exec ${nodejs}/bin/node --use_strict ${mqtt-mailer.build}/lib/node_modules/mqtt-mailer/src/app.js";
 }
