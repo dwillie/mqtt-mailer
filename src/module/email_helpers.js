eed803b26298dd6f7eb52b8e9e2e6786aa8bef07
Ewoudt Kellerman <ewoudt.kellerman@gmail.com>
2016-09-14 14:39:49 +1000

    FIX: Correctly use updated authentication module

--- src/module/email_helpers.js
+++ src/module/email_helpers.js
@@ -16,25 +16,33 @@ function validate(emailRequest) {
 }
 
 function send(message) {
-  return new Promise((fulfill) =>  {
-    const options = auth.centralOptions;
-    if (!validate(message)) {
-      throw Error('Invalid request');
-    }
-
-    options.json = { message };
-    options.method = 'POST';
-    request(getMailRequestUrl(), options, (err, response, body) => {
-      if (err) {
-        throw err;
-      } else if (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 202) {
-        winston.info(`Email accepted: ${JSON.stringify(body)}`);
-        fulfill(body);
-      } else if (response.statusCode === 401) {
-        winston.error('You are unauthorised');
-      } else {
-        winston.error(`Something else happened: ${response.statusCode}, body: ${JSON.stringify(body)}`);
+  return new Promise((fulfill, reject) => {
+    winston.info('getting central options..');
+    return auth.getCentralOptions
+    .then((centralOptions) => {
+      winston.info('obtaining token...');
+      winston.info(`obtained token: ${centralOptions.headers.Authorization}`);
+      const options = centralOptions;
+      if (!validate(message)) {
+        throw Error('Invalid request');
       }
+      options.json = { message };
+      options.method = 'POST';
+      winston.info(`Using detox central address: ${getMailRequestUrl()}`);
+      request(getMailRequestUrl(), options, (err, response, body) => {
+        if (err) {
+          throw err;
+        } else if (response.statusCode === 200 || response.statusCode === 201 || response.statusCode === 202) {
+          winston.info(`Email accepted: ${JSON.stringify(body)}`);
+          fulfill(body);
+        } else if (response.statusCode === 401) {
+          winston.error('You are unauthorised');
+          reject('You are unauthorised');
+        } else {
+          winston.error(`Something else happened: ${response.statusCode}, body: ${JSON.stringify(body)}`);
+          reject(`Something else happened: ${response.statusCode}, body: ${JSON.stringify(body)}`);
+        }
+      });
     });
   });
 }
